9e391c3e9966f5150b068fe20ba19a034bea89d5
prajwal-codes <prajwalkh09@gmail.com>
2023-04-04 23:56:11 +0530

    feat: adding new learnings

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // let mut x= 65;
+    // println!("The value of x if {}", x);
+
+    // x=60;
+    // println!("The value of x if {}", x);
+
+    // println!("Hello, gavinwood!");
+
+    // this is a comment
+    /*This 
+    is 
+    also
+    a comment
+    in multi lines */
+
+    // let x:u64 =45; unsigned int
+    // let x:i64 =66; signed int of 64 bit
+    // let f:f32 =66.67;
+    // let b:bool = false;
+
+    let n =45;
+
+    if n==45 {
+        println!("No  is greater than  30");
+}
+}
